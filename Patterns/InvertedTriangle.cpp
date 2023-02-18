2fe6cb6ab6cbd5fa3807b1a0afa5303acedb980b
SK-here <trivedisaksham@gmail.com>
2023-02-18 20:58:22 +0530

    Inverted Triangle Pattern

---
+++ Patterns/InvertedTriangle.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file Inverted-Triangle
+ * @author SK-here (trivedisaksham@gmail.com)
+ * @brief A program to print Inverted Triangle Pattern
+ * @version 0.1
+ * 
+ * Output:
+ *     
+ *      * * * * * 
+ *      *     * 
+ *      *   * 
+ *      * * 
+ *      * 
+ * 
+ */
+#include <iostream>
+#include <string.h>
+
+int main(){
+
+    std::cout <<"Enter the number: ";    
+    int num{0};
+    std::cin >>num;
+
+    // For Rows
+    for (int row = 0; row < num; row++){
+
+        //For Coulmn
+        for (int col = row; col < num; col++){
+                std::cout <<"* ";
+        }
+
+       std::cout <<'\n';
+    }
+}
