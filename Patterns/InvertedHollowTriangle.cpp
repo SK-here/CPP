56fe25b05b0b78fe30f825e596d7e8bd29d2b796
SK-here <trivedisaksham@gmail.com>
2023-02-18 20:58:46 +0530

    Inverted Hollow Triangle Pattern

---
+++ Patterns/InvertedHollowTriangle.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file Inverted-Triangle
+ * @author SK-here (trivedisaksham@gmail.com)
+ * @brief A program to print Hollow Inverted Triangle Pattern
+ * @version 0.1
+ * 
+ * Output:
+ *     
+ *      * * * * * 
+ *      *     * 
+ *      *   * 
+ *      * * 
+ *      * 
+ * 
+ */
+#include <iostream>
+#include <string.h>
+
+int main(){
+
+    std::cout <<"Enter the number: ";    
+    int num{0};
+    std::cin >>num;
+
+    // For Rows
+    for (int row = 0; row < num; row++){
+
+        //For Coulmn
+        for (int col = row; col < num; col++){
+            if(row == 0 || col == row || row == num - 1 || col == num - 1)
+                std::cout <<"* ";
+            else std::cout <<"  ";
+        }
+
+       std::cout <<'\n';
+    }
+}
