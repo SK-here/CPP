3f7eab0078a87cdf8a0d1b7722669558dfd36a07
SK-here <trivedisaksham@gmail.com>
2023-02-20 00:06:36 +0530

    Right Angled Triangle

---
+++ Patterns/RightTriangle.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file Pyramid.cpp
+ * @author SK-here (trivedisaksham@gmail.com)
+ * @brief program to print pyramid using '*' 
+ * @version 0.1
+ * 
+ * 
+ */
+
+# include <iostream>
+int main(){
+    int num{5};
+
+    for (int row = 0; row <= num; row++){
+        for (int col = row ; col >= 1; col--){
+            std::cout <<"* ";
+        }
+       std::cout <<'\n';
+    }
+}
