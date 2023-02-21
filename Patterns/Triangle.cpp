5f74d15e7ef9437d00558c13277607aeb73a97bf
SK-here <trivedisaksham@gmail.com>
2023-02-22 00:17:32 +0530

    Triangle

--- Patterns/Triangle.cpp
+++ Patterns/Triangle.cpp
@@ -1,6 +1,27 @@
+/**
+ * @file Triangle.cpp
+ * @author SK-here (trivedisaksham@gmail.com)
+ * @brief A program to print Triangle
+ * @version 0.1
+ *
+ *  Output:
+ * 
+ *            * 
+ *          * * 
+ *        * * * 
+ *      * * * * 
+ *    * * * * * 
+ * 
+ */
 #include <iostream>
 int main(){
-    int num{5};
+    
+    std::cout <<"Enter the Number: ";
+
+    int num{0};
+
+    std::cin >>num;
+
 
     for (int row = 0; row < num; row++){
         for (int col = row; col < num; col++){
