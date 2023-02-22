178992edf7a107f2f8a4f5cc583d1c072089c339
SK-here <trivedisaksham@gmail.com>
2023-02-22 18:16:28 +0530

    Downwards Arrow Pattern

---
+++ Patterns/DownwardArrow.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file LeftArrow.cpp
+ * @author SK-here (trivedisaksham@gmail.com)
+ * @brief A program to print Left Arrow 
+ * @version 0.1
+ * Output:
+ *        # # # #       
+ *        # # # #       
+ *        # # # #       
+ *        # # # #       
+ *        # # # #       
+ *  # # # # # # # # # # 
+ *    # # # # # # # # 
+ *      # # # # # # 
+ *        # # # # 
+ *          # #  
+ * 
+ */
+# include <iostream>
+int main(){
+    
+    std::cout <<"Enter the number: ";
+    int num{0};
+    std::cin >>num;
+    
+    std::cout <<'\n';
+
+    for (int row = 0 ; row < num ; row++){
+        
+        for (int col = 0 ; col < num ; col++){
+            col == num -1 || col == num - 2 ? std::cout <<"# " : std::cout <<"  ";
+        }
+        
+        for (int col = 0; col < num ; col++)
+        {
+            col == 0 || col == 1 ? std::cout <<"# " : std::cout <<"  ";
+        }
+        
+        std::cout <<'\n';
+    }
+    
+    for (int row = 0; row < num ; row++){
+    
+       for (int col = 0 ; col < row ; col++){
+            std::cout <<"  ";
+        }
+        for (int col = row ; col < num ; col++){
+            std::cout <<"# ";
+        }
+        for (int col = row ; col < num ; col++){
+            std::cout <<"# ";
+        }
+        std::cout <<'\n';
+    }
+}
